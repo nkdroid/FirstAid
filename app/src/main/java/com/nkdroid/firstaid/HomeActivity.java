380c34d4c76733863538bcf48e722c93c4e72fce
nirav kalola <niravnvk@gmail.com>
2015-03-22 07:47:56 +0530

    dgf

--- app/src/main/java/com/nkdroid/firstaid/HomeActivity.java
+++ app/src/main/java/com/nkdroid/firstaid/HomeActivity.java
@@ -38,6 +38,8 @@ public class HomeActivity extends ActionBarActivity {
                 startActivity(intent);
             }
         });
+
+
         setToolbar();
         initView();
         getRegId();
